92c7555c7a6131cafa61eb9b32e9f80e3d0569ed
Rex <rexcodeworks@gmail.com>
2023-06-10 18:50:42 -0700

    fix class error

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -3,18 +3,18 @@ function MovieDetail({ movie, onClose }) {
 
     return (
         <div>
-            <div class="columns">
-                <div class="column is-one-third">
+            <div className="columns">
+                <div className="column is-one-third">
                     <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} />
                 </div>
-                <div class="column">
+                <div className="column">
                     <h1>{title}</h1>
                     <p>Release Date: {release_date}</p>
                     <p>Vote Average: {vote_average}</p>
                     <p>{overview}</p>
-                    <button class="button is-danger is-small" onClick={onClose}>
-                        <span class="icon is-left">
-                            <i class="fas fa-times"></i>
+                    <button className="button is-danger is-small" onClick={onClose}>
+                        <span className="icon is-left">
+                            <i className="fas fa-times"></i>
                         </span>
                         Close
                     </button>
